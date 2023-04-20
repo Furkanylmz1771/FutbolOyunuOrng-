f68be44475dae3f8953d9974f9c3fe59cd0a8b30
Furkan Yılmaz <92302477+Furkanylmz1771@users.noreply.github.com>
2023-04-20 21:57:38 +0300

    Add files via upload

---
+++ FutbolUygGüncel/Futbol.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futbol
+{
+    public class Futbol
+    {
+        Random Randomr = new Random();
+
+        protected string AdSoyad;
+        protected int FormNo;
+        protected int Hız;
+        protected int Dayanıklılık;
+        protected int Pas, Şut, Yetenek;
+        protected int Kararlılık;
+        protected int DoğalForm;
+        protected int Şans;
+        public Futbol(string AdSoyad, int FormNo)       // Kurucu metod oluşturuldu
+        {
+
+            this.AdSoyad = AdSoyad;
+            this.FormNo = FormNo;
+
+            Hız = Randomr.Next(50, 100);             // 50 100 arasında rastgele sayı verir, 100 dahil değil.
+            Dayanıklılık = Randomr.Next(50, 100);
+            Pas = Randomr.Next(50, 100);
+            Şut = Randomr.Next(50, 100);
+            Yetenek = Randomr.Next(50, 100);
+            Kararlılık = Randomr.Next(50, 100);
+            DoğalForm = Randomr.Next(50, 100);
+            Şans = Randomr.Next(50, 100);
+
+        }
+
+        public Futbol() { }
+
+
+        public virtual Boolean Pasver()
+        {
+
+            int PasSkor = Convert.ToInt32(Pas * 0.3 + Yetenek * 0.3 + Dayanıklılık * 0.1 + DoğalForm * 0.1 + Şans * 0.2);
+
+            if (PasSkor > 60)
+
+                return true;
+
+            else
+
+                return false;
+
+
+        }
+
+        public virtual Boolean GolVurusu()
+        {
+            int GolSkor = Convert.ToInt32(Yetenek * 0.3 + Şut * 0.2 + DoğalForm * 0.1 + Kararlılık * 0.2 + Hız * 0.1 + Şans * 0.2);
+
+            if (GolSkor > 70)
+                return true;
+            else
+                return false;
+        }
+
+
+
+
+        public class Defans : Futbol
+        {
+            private int PozisyonAlma;
+            private int Kafa;
+            private int Sıcrama;
+            public Defans(string AdSoyad, int FormNo)
+            {
+                this.AdSoyad = AdSoyad;
+                this.FormNo = FormNo;
+                Hız = Randomr.Next(50, 90);
+                Dayanıklılık = Randomr.Next(50, 100);
+                Pas = Randomr.Next(50, 100);
+                Şut = Randomr.Next(50, 100);
+                Yetenek = Randomr.Next(50, 100);
+                Kararlılık = Randomr.Next(50, 100);
+                DoğalForm = Randomr.Next(50, 100);
+                Şans = Randomr.Next(50, 100);
+
+            }
+
+            public override Boolean Pasver()
+            {
+
+                int PasSkor = Convert.ToInt32(Pas * 0.3 + Yetenek * 0.3 + Dayanıklılık * 0.1 + PozisyonAlma * 0.1 + DoğalForm * 0.1 + Şans * 0.2);
+
+                if (PasSkor > 60)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+
+        }
+        public class OrtaSaha : Futbol
+        {
+            private int UzunTop;
+            private int İlkDokunuş;
+            private int Uretkenlik;
+            private int TopSurme;
+            private int OzelYetenek;
+            public OrtaSaha(string AdSoyad, int FormNo)
+            {
+                this.AdSoyad = AdSoyad;
+                this.FormNo = FormNo;
+                Hız = Randomr.Next(50, 90);
+                Dayanıklılık = Randomr.Next(50, 100);
+                Pas = Randomr.Next(50, 100);
+                Şut = Randomr.Next(50, 100);
+                Yetenek = Randomr.Next(50, 100);
+                Kararlılık = Randomr.Next(50, 100);
+                DoğalForm = Randomr.Next(50, 100);
+                Şans = Randomr.Next(50, 100);
+            }
+
+            public virtual Boolean Pasver()     // Virtual metotlar Inheritance (Kalıtım) yoluyla aktarıldıkları sınıfların
+                                                // içerisinden override edilerek değiştirilebilirler.
+                                                // Eğer override edilmezlerse temel sınıf içerisinde tanımladıkları şekilde çalışırlar.
+            {
+
+                int PasSkor = Convert.ToInt32(Pas * 0.3 + Yetenek * 0.3 + Dayanıklılık * 0.1 + DoğalForm * 0.1 + Şans * 0.2);
+
+                if (PasSkor > 60)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+
+
+            }
+
+
+        }
+
+        public class Forvet : Futbol
+        {
+            private int Bıtırıcılık;
+            private int IlkDokunus;
+            private int Kafa;
+            private int OzelYetenek;
+            private int Soğukkanlılık;
+            public Forvet(string AdSoyad, int FormNo)
+            {
+                this.AdSoyad = AdSoyad;
+                this.FormNo = FormNo;
+                Hız = Randomr.Next(50, 90);
+                Dayanıklılık = Randomr.Next(50, 100);
+                Pas = Randomr.Next(50, 100);
+                Şut = Randomr.Next(50, 100);
+                Yetenek = Randomr.Next(50, 100);
+                Kararlılık = Randomr.Next(50, 100);
+                DoğalForm = Randomr.Next(50, 100);
+                Şans = Randomr.Next(50, 100);
+            }
+
+
+            public virtual Boolean Pasver()
+            {
+
+                int PasSkor = Convert.ToInt32(Pas * 0.3 + Yetenek * 0.3 + Dayanıklılık * 0.1 + DoğalForm * 0.1 + Şans * 0.2);
+
+                if (PasSkor > 60)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+
+            }
+        }
+    }
+}
---
+++ FutbolUygGüncel/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Futbol.Futbol;
+
+namespace Futbol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Futbol> takım = new List<Futbol>    // Futboldan değerler alan takım adında  dizi oluşturuldu
+            {
+                new Futbol("kaleci", 1),       // Takım dizisinin ilk indeksine 1  nolu kaleci eklendi
+                new Defans("defans1", 2),
+                new Defans("defans2", 3),
+                new Defans("defans3", 4),
+                new Defans("defans4", 5),
+                new OrtaSaha("OrtaSaha1", 6),
+                new OrtaSaha("OrtaSaha2", 7),
+                new OrtaSaha("OrtaSaha3", 8),
+                new OrtaSaha("OrtaSaha4", 9),
+                new Forvet("Forvet1", 10),
+                new Forvet("Forvet2", 11)
+            };
+
+
+            Random rastgele = new Random();
+            int FormaNo;
+            bool GolOlabilir = true;
+            FormaNo = rastgele.Next(1, 12);
+
+
+
+            for (int i = 1; i <= 3; i++)
+            {
+                FormaNo = rastgele.Next(1, 12);       // 12 dahil değil.
+                if (!takım[FormaNo - 1].Pasver())     //  -1 = diziler 0'dan başlar.'    
+                {
+                    Console.WriteLine(FormaNo + "numaralı oyuncu pası yanlış");
+                    GolOlabilir = false;      // Hatalı pas için
+                }
+                else
+                {
+                    Console.WriteLine(FormaNo + "numaralı oyuncu pası doğru");
+
+                }
+            }
+
+            if (GolOlabilir = true)
+            {
+                if (takım[FormaNo - 1].GolVurusu())
+
+                    Console.WriteLine("Gol oldu");
+
+                else
+                {
+                    Console.WriteLine("Gol vuruşu başarısız.");
+
+                    Console.ReadLine();
+
+
+                }
+
+            }
+        }
+    }
+}
